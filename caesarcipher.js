c5fe13d9d2d0c1a3aa40c298c54b3834d26fb195
Tee <bunth667@gmail.com>
2018-01-14 18:38:58 -0500

    cipher done

---
+++ caesarcipher.js
@@ -0,0 +1,27 @@
+function caesarCipher(str, num) {
+  num = num % 26;
+  let lowerCaseString = str.toLowerCase();
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  let newString = '';
+
+  for(let i = 0; i < lowerCaseString.length; i++) {
+    let currentLetter = lowerCaseString[i];
+    if(currentLetter === ' ') {
+      newString += currentLetter;
+      continue;
+    }
+    let currentIndex = alphabet.indexOf(currentLetter);
+    let newIndex = currentIndex + num;
+    if( newIndex > 25 ) newIndex = newIndex - 26;
+    if( newIndex < 0 ) newIndex = newIndex + 26;
+    if( str[i] === str[i].toUpperCase()) {
+      newString += alphabet[newIndex].toUpperCase();
+    } else {
+      newString += alphabet[newIndex];
+    }
+  }
+  return newString;
+}
+
+let result = caesarCipher('cat', 2);
+console.log(result);
